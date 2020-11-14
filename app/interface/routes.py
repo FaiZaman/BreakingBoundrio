92ca8424615d38f38fa50eb4935966dbc633de01
johnny-sa <32818071+johnny-sa@users.noreply.github.com>
2020-11-14 19:34:17 +0000

    added some routes to query
    - checks if cell id is broken
    
    - saves current cell id

--- app/interface/routes.py
+++ app/interface/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, current_app, abort, request
 from functools import wraps
 from app.db import User
+from app.db import Hex 
+import time  
 
 
 bp = Blueprint('interface', __name__)
@@ -23,3 +25,32 @@ def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
+
+@bp.route('/World/check/<id>')
+def is_broken(id):
+    t = Hex.query.filter_by(id=id).first()
+    if  time.time() - t.broken > time.timedelta(s = 60):
+      return True
+    if t.broken is None:
+      t.broken = time.time() 
+      db.session.commit()
+      return False
+    else:
+      return False
+
+@bp.route('/World/save/<id>')
+def save_cell(id):
+    if current_user.is_authenticated:
+      user = User.query.filter_by(username=username).first()
+      user.save_point = Hex.id
+      db.session.commit()
+      return redirect("/index")
+
+
+
+
+
+
+    
+
+
