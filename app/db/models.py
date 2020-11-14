6ec3e1ed62853554095b5264ad934ff577af50f2
johnny-sa <32818071+johnny-sa@users.noreply.github.com>
2020-11-14 18:05:04 +0000

    Created Hex Class

--- app/db/models.py
+++ app/db/models.py
@@ -8,6 +8,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    high_score = db.Column(db.Integer, index = True)   ###Compare this to previous high score of another user
+                                                       ###check if higher then replace other user
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -21,6 +23,8 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in}, Config.SECRET_KEY, algorithm='HS256').decode('utf-8')
 
+
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
@@ -28,3 +32,20 @@ class User(UserMixin, db.Model):
         except:
             return
         return User.query.get(id)
+
+
+class Hex(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    broken = db.Column(db.Time, index=True) ###datetime.time objects i.e. if in past more than 't' then it is still broken 
+
+    def __repr__(self):
+        return self.id
+
+    def breaking(self):
+        self.broken = datetime.time()
+
+
+        
+
+
+    
