228a88f6793c851c1e7187117ad984b9925f354f
bamiesking <bamiesking@gmail.com>
2020-11-14 18:16:29 +0000

    Added decorator to compare handshake secret in request headers to SECRET variable in config file
    This secret will need to be put somewhere... secret (I guess?)

--- app/interface/routes.py
+++ app/interface/routes.py
@@ -5,6 +5,15 @@ from app.db import User
 
 bp = Blueprint('interface', __name__)
 
+def validate_request(f):
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+    data = request.headers.get('handshake')
+    if not data == current_app.config['SECRET']:
+      abort(401)
+    return f(*args, **kwargs)
+  return decorated_function
+
 @bp.route('/')
 def status():
     return 'True'
