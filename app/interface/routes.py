8cd8d936928e10d6a265a6d506789b02728f1162
bamiesking <bamiesking@gmail.com>
2020-11-14 20:16:53 +0000

    Edited interface view functions, and fixed some minor indentation

--- app/interface/routes.py
+++ app/interface/routes.py
@@ -1,20 +1,20 @@
-from flask import Blueprint, current_app, abort, request
+from flask import Blueprint, current_app, abort, request, jsonify
 from functools import wraps
 from app.db import User
-from app.db import Hex
-import time
+from app.db import Hex, db
+from datetime import datetime, timedelta
 
 
 bp = Blueprint('interface', __name__)
 
 def validate_request(f):
-  @wraps(f)
-  def decorated_function(*args, **kwargs):
-    data = request.headers.get('handshake')
-    if not data == current_app.config['SECRET']:
-      abort(401)
-    return f(*args, **kwargs)
-  return decorated_function
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        data = request.headers.get('handshake')
+        if not data == current_app.config['SECRET']:
+            abort(401)
+        return f(*args, **kwargs)
+    return decorated_function
 
 @bp.route('/')
 def status():
@@ -29,21 +29,33 @@ def user(username):
 @bp.route('/World/check/<id>')
 def is_broken(id):
     t = Hex.query.filter_by(id=id).first()
-    if  time.time() - t.broken > time.timedelta(s = 60):
-      return True
+    if t is None:
+        abort(404)
     if t.broken is None:
-      t.broken = time.time() 
-      db.session.commit()
-      return False
+        t.broken = time.time() 
+        db.session.commit()
+    if datetime.now() - datetime.utcfromtimestamp(t.broken) > timedelta(seconds = 60):
+        return jsonify(True)
     else:
-      return False
+        return jsonify(False)
 
-@bp.route('/World/save/<id>')
-def save_cell(id):
-    if current_user.is_authenticated:
-      user = User.query.filter_by(username=username).first()
-      user.save_point = Hex.id
-      db.session.commit()
-      return redirect("/index")
+@bp.route('/user/<username>/set_position/<id>')
+def set_position(id):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    user.save_point = id
+    db.session.commit()
+    return '', 200
+
+@bp.route('/World/create/<id>')
+def create_hex(id):
+    t = Hex.query.filter_by(id=id).first()
+    if t is not None:
+        abort(404)
+    t = Hex(broken=datetime.now().timestamp())
+    db.session.add(t)
+    db.session.commit()
+    return '', 200
 
 
