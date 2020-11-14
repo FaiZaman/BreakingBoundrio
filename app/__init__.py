574b49c634c7e07e34f3107fc50bd1d5ee5bba42
bamiesking <bamiesking@gmail.com>
2020-11-14 14:47:14 +0000

    Setup database
    Added db blueprint to provide database access and created User model

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
 
+from config import Config
+
 from app.main import bp as main_bp
+from app.db import db
 
 
-def create_app():
-    # Setup flask app
-    app = Flask(__name__)
+def create_app(config_class=Config):
+    # Setup flask app and configure from config object
+    app = Flask(__name__, template_folder='../staticPages')
+    app.config.from_object(config_class)
 
     # Register blueprints
     app.register_blueprint(main_bp)
+
+    # Initialise database object
+    db.init_app(app)
 
     return app
