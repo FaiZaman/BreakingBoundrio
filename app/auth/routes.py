90f1f887bb4c7fb690f14d9c8360826206107f68
bamiesking <bamiesking@gmail.com>
2020-11-14 14:52:14 +0000

    Tidied up imports in app.auth.routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,10 +1,7 @@
-from flask import render_template, flash, redirect, url_for, Blueprint, current_app, request
+from flask import render_template, flash, redirect, url_for, Blueprint
 from flask_login import current_user, login_user, logout_user
-from .models import User, db
-from app.forms import LoginForm, RegistrationForm, ResetPasswordRequestForm, ResetPasswordForm
-from app.email import send_password_reset_email
-import traceback
-import json
+from app.db import User, db
+from .forms import LoginForm, RegistrationForm
 
 
 bp = Blueprint('main', __name__)
