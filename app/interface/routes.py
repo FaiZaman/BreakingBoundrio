8462531e98e1fb01d5ead8dccbbb7a3080ecd0f3
bamiesking <bamiesking@gmail.com>
2020-11-14 18:15:08 +0000

    Added example routes to interface blueprint

--- app/interface/routes.py
+++ app/interface/routes.py
@@ -1,8 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, current_app, abort, request
+from functools import wraps
+from app.db import User
 
 
 bp = Blueprint('interface', __name__)
 
 @bp.route('/')
 def status():
-    return True
+    return 'True'
+
+@bp.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
